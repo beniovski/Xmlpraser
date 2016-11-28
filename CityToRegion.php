9bdcfd03ba22967f91f18f8856cc0dac688946a5
daniel <daniel.bednarczuk@paretti.pl>
2016-11-28 13:39:56 +0100

    add CityToRegion API

---
+++ CityToRegion.php
@@ -0,0 +1,42 @@
+<?php
+
+class CityToRegion 
+{
+	private  $db_user = "opole_32";
+	private  $db_name = "opole_32";
+	private  $db_host = "sql.opole.nazwa.pl";
+	private  $db_pass = "P@retti1!";
+	protected $con;
+	
+	private function dbConnection()
+	{
+		$utf8name = "SET NAMES utf8";                   										    // ustawienie kodowania
+		$utf8char = "SET CHARACTER_SET utf8_polish_ci";
+	
+		$this->con = @new mysqli($this->db_host,$this->db_name,$this->db_pass,$this->db_name);
+		if ($this->con->connect_errno!=0)
+		{
+			echo $this->con->connect_errno;
+		}
+	
+		$result = $this->con->query($utf8name);
+		$result = $this->con->query($utf8char);	
+	
+	}
+	
+	
+	 function Region($city)
+	{
+	 	$this->dbConnection();
+	 		
+		$query = "SELECT region  FROM `city` WHERE city = '$city'";
+		$result = $this->con->query($query);
+		$result = $result->fetch_assoc();
+		$result = $result['region'];
+		return $result;
+		
+	}
+	
+	
+	
+}
